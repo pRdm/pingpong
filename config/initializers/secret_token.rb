c402761933d82b55b0378e390ee938882c3a5da5
Woping Li <new39390@hotmail.com>
2014-06-02 09:26:19 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pingpong::Application.config.secret_key_base = '5dbd6b40b73ee0ce473339b72a4a5885594c6008930665161a27b58064d2f55516b6213afb13981b3c3c837737625adf68e7e5a9587ac06dbd82b52cb5ac2cb6'
